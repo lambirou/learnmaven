9600e19193d2f611de9019b8b259ae8aa7dbb7c0
Roland Edi <lambirou225@gmail.com>
2021-12-09 14:08:34 +0000

    First commit

---
+++ src/test/java/com/lambirou/learnjava/AppTest.java
@@ -0,0 +1,18 @@
+package com.lambirou.learnjava;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
